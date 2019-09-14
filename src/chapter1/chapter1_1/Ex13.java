d97415b6e9c0a41c76dd513541ec9ecd139b6f6d
Zhou Zhenzhou <Zhenzhou.zhou@linde-china.com>
2019-09-14 09:34:05 +0800

    Exercise 1.1.13

---
+++ src/chapter1/chapter1_1/Ex13.java
@@ -0,0 +1,17 @@
+package chapter1.chapter1_1;
+
+public class Ex13 {
+  public static void printTransposedMatrix(int[][] matrix) {
+    for (int i = 0; i < matrix[0].length; i++) {
+      for (int j = 0; j < matrix.length; j++) {
+        System.out.printf("%6d", matrix[j][i]);
+      }
+      System.out.println();
+    }
+  }
+
+  public static void main(String[] args) {
+    int[][] a = { { 100, 200, 300 }, { 400, 500, 600 }, { 700, 800, 900 }, { 1000, 1100, 1200 } };
+    printTransposedMatrix(a);
+  }
+}

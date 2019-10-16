a53df2af4a9e04493bde3b2cc2c4c09fa848550b
Zhou Zhenzhou <275028888@qq.com>
2019-10-16 21:52:01 +0800

    Exercise 1.2.4

---
+++ src/chapter1/chapter1_2/Ex04.java
@@ -0,0 +1,14 @@
+package chapter1.chapter1_2;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class Ex04 {
+  public static void main(String[] args) {
+    String s1 = "Hello";
+    String s2 = s1;
+    s1 = "World";
+
+    StdOut.println(s1);
+    StdOut.println(s2);
+  }
+}

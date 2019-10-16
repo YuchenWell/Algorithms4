e3e456643987a28ae622c295e6f6ca1885bfaff7
Zhou Zhenzhou <275028888@qq.com>
2019-10-16 21:58:59 +0800

    Exercise 1.2.5

---
+++ src/chapter1/chapter1_2/Ex05.java
@@ -0,0 +1,13 @@
+package chapter1.chapter1_2;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class Ex05 {
+  public static void main(String[] args) {
+    String s = "Hello World";
+    s.toUpperCase();
+    s.substring(6,11);
+
+    StdOut.println(s);
+  }
+}

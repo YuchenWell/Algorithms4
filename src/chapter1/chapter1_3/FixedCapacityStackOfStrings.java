e016311346014bdb8a3a30f21010473ff3cc0629
Zhou Zhenzhou <275028888@qq.com>
2020-01-01 22:52:53 +0800

    Exercise 1.3.1

---
+++ src/chapter1/chapter1_3/FixedCapacityStackOfStrings.java
@@ -0,0 +1,56 @@
+package chapter1.chapter1_3;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class FixedCapacityStackOfStrings {
+  private String[] a;
+  private int N;
+
+  public FixedCapacityStackOfStrings(int cap) {
+    a = new String[cap];
+  }
+
+  public boolean isEmpty() {
+    return N == 0;
+  }
+
+  public int size() {
+    return N;
+  }
+
+  public void push(String item) {
+    a[N++] = item;
+  }
+
+  public String pop() {
+    return a[--N];
+  }
+
+
+  /**
+   * Ex1.3.1
+   */
+  public boolean isFull() {
+    return N == a.length;
+  }
+
+  public static void main(String[] args) {
+    FixedCapacityStackOfStrings s;
+    s = new FixedCapacityStackOfStrings(100);
+
+    while (!StdIn.isEmpty()) {
+      String item = StdIn.readString();
+      if (!item.equals("-")) {
+        s.push(item);
+      } else {
+        s.pop();
+      }
+      StdOut.println("(" + s.size() + " left on stack)");
+    }
+
+  }
+
+}
+
+

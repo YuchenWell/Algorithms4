70b33ae09abeeb12e1ea9cc1ae445ef5c92c9ac3
Zhou Zhenzhou <275028888@qq.com>
2020-01-29 19:19:03 +0800

    Ex 1.3.36

--- src/chapter1/chapter1_3/RandomQueue.java
+++ src/chapter1/chapter1_3/RandomQueue.java
@@ -1,13 +1,20 @@
 package chapter1.chapter1_3;
 
 import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+import java.util.Iterator;
 
 /*************************************************************************
  *  Exercise 1.3.35
  *  随机队列
+ *
+ *  Exercise 1.3.36
+ *  随机迭代器
+ *  Fisher–Yates shuffle 洗牌算法
  *************************************************************************/
 
-public class RandomQueue<Item> {
+public class RandomQueue<Item> implements Iterable<Item> {
   private Item[] randomQueue = (Item[]) new Object[1];
   private int N = 0;
 
@@ -51,6 +58,30 @@ public class RandomQueue<Item> {
     return randomQueue[random];
   }
 
+  public Iterator<Item> iterator() {
+    return new RandomQueueIterator();
+  }
+
+  private class RandomQueueIterator implements Iterator<Item> {
+    private int n = N;
+
+    public RandomQueueIterator() {
+      StdRandom.shuffle(randomQueue);
+    }
+
+    public boolean hasNext() {
+      return n > 0;
+    }
+
+    public void remove() {
+
+    }
+
+    public Item next() {
+      return randomQueue[--n];
+    }
+  }
+
 
   public static void main(String[] args) {
     RandomQueue<String> randomQueue = new RandomQueue<String>();
@@ -60,6 +91,12 @@ public class RandomQueue<Item> {
     randomQueue.enqueue("C");
     randomQueue.enqueue("D");
 
+    StdOut.println("Iterator: ");
+    for (String string : randomQueue) {
+      StdOut.print(string);
+    }
+
+    StdOut.println();
     StdOut.println("Sample (6x): ");
     StdOut.print(randomQueue.sampLe());
     StdOut.print(randomQueue.sampLe());
@@ -68,6 +105,7 @@ public class RandomQueue<Item> {
     StdOut.print(randomQueue.sampLe());
     StdOut.print(randomQueue.sampLe());
 
+    StdOut.println();
     StdOut.println("Dequeue (6x): ");
     StdOut.print(randomQueue.dequeue());
     StdOut.print(randomQueue.dequeue());
